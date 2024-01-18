09c0a78fefd04e2021de74f34c230b93964c3c30
Pungky Kristianto <pungky-kristianto@c-fo.com>
2024-01-18 10:11:48 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pungky95/refiner
+
+go 1.19
+
+require golang.org/x/exp v0.0.0-20240112132812-db7319d0e0e3 // indirect
---
+++ refiner.go
@@ -0,0 +1,21 @@
+package filter
+
+import "golang.org/x/exp/constraints"
+
+type AllowedTypes interface {
+	constraints.Ordered
+}
+
+func Refine[T AllowedTypes](slice1, slice2 []T) []T {
+	combinedSlice := append(slice1, slice2...)
+	temp := make(map[T]bool)
+	var refined []T
+	for _, v := range combinedSlice {
+		_, ok := temp[v]
+		if !ok {
+			temp[v] = true
+			refined = append(refined, v)
+		}
+	}
+	return refined
+}
